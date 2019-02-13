b2166d9528a9c9376bb19ee3539351d07984c98d
Ravi Kiran Kasula <rkasul@codeaurora.org>
2019-02-13 11:15:14 -0800

    Convert qcom.fmradio into Android.bp module
    Modules in PRODUCT_BOOT_JARS need to be Android.bp modules.
    
    Bug: 124520734
    Change-Id: Icd858816b1f0cb53ec0fcb40f0aa73ce80983676
    Signed-off-by: theimpulson <aayushgupta219@gmail.com>

--- Android.mk
+++ Android.mk
@@ -2,18 +2,6 @@ ifeq ($(BOARD_HAVE_QCOM_FM),true)
 
 LOCAL_PATH:= $(call my-dir)
 LOCAL_DIR_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_MODULE_TAGS := optional
-
-
-LOCAL_SRC_FILES := $(call all-java-files-under, qcom/fmradio)
-LOCAL_JNI_SHARED_LIBRARIES := libqcomfm_jni
-
-LOCAL_SHARED_LIBRARIES := liblog
-LOCAL_MODULE:= qcom.fmradio
-
-include $(BUILD_JAVA_LIBRARY)
 
 ifeq ($(BOARD_HAS_QCA_FM_SOC), "cherokee")
 LOCAL_CFLAGS += -DFM_SOC_TYPE_CHEROKEE
